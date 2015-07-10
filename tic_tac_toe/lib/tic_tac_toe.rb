3df600c380c4caaa70054527e572b0268cdf8de5
xreid <xreid@outlook.com>
2015-07-09 22:34:28 -0700

    added executable for tic tac toe

--- tic_tac_toe/lib/tic_tac_toe.rb
+++ tic_tac_toe/lib/tic_tac_toe.rb
@@ -6,9 +6,4 @@ require_relative 'tic_tac_toe/game.rb'
 require_relative 'tic_tac_toe/exceptions.rb'
 
 module TicTacToe
-  # comment out to run tests
-  # game = Game.new
-  # game.get_players
-  # Game.instructions
-  # game.play
 end
